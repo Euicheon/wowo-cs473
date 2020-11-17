d31dd546e37d79eb75c6cf2ad76ac4787260dcff
Yoonseo Kim <grandchasevs@kaist.ac.kr>
2020-11-17 23:45:41 +0900

    Search

--- client/src/JSX/Posts.jsx
+++ client/src/JSX/Posts.jsx
@@ -2,7 +2,9 @@ import React, {useEffect, useState} from "react";
 import axios from "axios";
 import InfiniteScroll from 'react-infinite-scroll-component';
 import Masonry from 'react-masonry-css';
+import '../CSS/posts.css'
 
+import {Input} from 'mdbreact';
 import {Loader} from '../components/Loader';
 import Post from './Post';
 
@@ -12,12 +14,14 @@ const headers = { withCredentials: true };
 const Posts = () => {
     
     const [posts, setPosts] = useState([]);
-    const [hasmore, setHasMore] = useState(true)
-    const [page, setPage] = useState(1)
+    const [allPosts, setAllPosts] = useState([]);
+    const [hasmore, setHasMore] = useState(true);
+    const [page, setPage] = useState(1);
 
-    const breakpointsColumnsObj = {default: 2, 1200: 3, 992: 3, 768: 2, 576: 1}
+    const breakpointsColumnsObj = {default: 2}
 
     useEffect(() => {
+        fetchAllPosts();
         fetchPosts();
     }, []);
 
@@ -27,32 +31,54 @@ const Posts = () => {
             .post("http://localhost:8080/post/getPostList", send_param)
             .then(returnData => {
                 if (returnData.data.length === 0) {
+                    console.log('fetchPostsFinished',returnData.data);
                     alert("All posts loaded");
                     setHasMore(false)}
                 else {
-                    setPosts((prev) => [...prev, ...returnData.data.posts])
-                    setPage(prev => prev + 1)
+                    console.log('fetchPosts',returnData.data);
+                    setPosts((prev) => [...prev, ...returnData.data.posts]);
+                    setPage(prev => prev + 1);      
                 }
             }).catch(err => {console.log(err);}); 
     };
 
+    const fetchAllPosts = () => {
+        const send_param = {headers, _id: 'myoons'};
+        axios
+            .post("http://localhost:8080/post/getAll", send_param)
+            .then(returnData => {
+                setAllPosts(returnData.data.posts)
+            }).catch(err => {console.log(err);}); 
+    };
+
+    const onChange = evt => {
+
+        let filteredPosts = allPosts.filter(post => {
+            return post.title.toLowerCase().includes(evt.target.value.toLowerCase());
+        })
+        setPosts(filteredPosts);
+    }
+
     return (
         <div className="row">
-            <div className="col-md-12">
-                <InfiniteScroll
-                    dataLength={posts.length}
-                    next={fetchPosts}
-                    hasMore={hasmore}
-                    loader={<Loader/>}>
+            <div>
+            <Input label="Serach Posts" onChange={onChange}/>
+                <div className="col-md-12">
+                    <InfiniteScroll
+                        dataLength={posts.length}
+                        next={fetchPosts}
+                        hasMore={hasmore}
+                        loader={<Loader/>}>
 
-                    <Masonry
-                        breakpointCols={breakpointsColumnsObj} 
-                        className="masonry-grid" 
-                        columnClassName="masonry-grid_column">
-                        {posts.map((post) => (<Post key={post._id} info={post}/>))}
-                    </Masonry>
+                        <Masonry
+                            breakpointCols={breakpointsColumnsObj} 
+                            className="masonry-grid" 
+                            columnClassName="masonry-grid_column">
+                            {posts.map((post) => (<Post key={post._id} info={post}/>))}
+                        </Masonry>
 
-                </InfiniteScroll>
+                    </InfiniteScroll>
+                </div>
             </div>
         </div>
     );
--- server/routes/postRouter.js
+++ server/routes/postRouter.js
@@ -2,6 +2,81 @@ const express = require("express");
 const router = express.Router();
 const Post = require("../schemas/post");
 
+// Write a new post & Per Page : 10 Posts 
+router.post("/write", async (req, res) => {
+  try {
+    let currentPage;
+    let obj;
+    let currentPagePosts;
+
+    const mostCurrentPost = await Post.findOne({}, null, {sort: { createdAt: -1 }});
+    
+    if (mostCurrentPost === null) {currentPage=1}
+    else {
+      currentPagePosts = await Post.find({page: mostCurrentPost.page})
+      if (currentPagePosts.length < 10) { currentPage = mostCurrentPost.page}
+      else { currentPage = mostCurrentPost.page + 1}
+    }
+
+
+    obj = {
+      writer: req.body._id,
+      title: req.body.title,
+      content: req.body.content,
+      page: currentPage,
+    };
+
+    const newPost = new Post(obj);
+    await newPost.save();
+    res.json({ message: "Post Uploaded" });
+  } catch (err) {
+    console.log(err);
+    res.json({ message: false });
+  }
+});
+
+// Get the posts in DB & Infinite Scroll
+router.post("/getPostList", async (req, res) => {
+
+  let currentPage;
+  let fetchLength;
+
+  try {
+    const mostCurrentPost = await Post.findOne({}, null, {sort: { createdAt: -1 }});
+    const fetchPosts = await Post.find({page: req.body._page}, null, {sort: { createdAt: -1 }})
+    
+    if (fetchPosts === null) {fetchLength=0}
+    else {fetchLength=fetchPosts.length}
+
+    res.json({ posts: fetchPosts, length: fetchLength});
+  } catch (err) {
+    console.log(err);
+    res.json({ message: false });
+  }
+});
+
+// Get the all posts in DB
+router.post("/getAll", async (req, res) => {
+  try {
+    const fetchPosts = await Post.find({}, null, {sort: { createdAt: -1 }});
+    res.json({posts: fetchPosts});
+  } catch (err) {
+    console.log(err);
+    res.json({ message: false });
+  }
+});
+
+router.post("/detail", async (req, res) => {
+  try {
+    const _id = req.body._id;
+    const detailedPost = await Post.find({ _id });
+    res.json({ detailedPost });
+  } catch (err) {
+    console.log(err);
+    res.json({ message: false });
+  }
+});
+
 router.post("/delete", async (req, res) => {
   try {
     await Post.remove({
@@ -26,58 +101,6 @@ router.post("/update", async (req, res) => {
       }
     );
     res.json({ message: "Post Edited" });
-  } catch (err) {
-    console.log(err);
-    res.json({ message: false });
-  }
-});
-
-// Write a new post
-router.post("/write", async (req, res) => {
-  try {
-    let currentPage;
-    let obj;
-
-    const mostCurrentPost = await Post.findOne({}, null, {sort: { createdAt: -1 }});
-    const currentPagePosts = await Post.find({page: mostCurrentPost.page});
-    
-    if (currentPagePosts.length < 8) { currentPage = mostCurrentPost.page}
-    else { currentPage = mostCurrentPost.page + 1}
-
-    obj = {
-      writer: req.body._id,
-      title: req.body.title,
-      content: req.body.content,
-      page: currentPage,
-    };
-
-    const newPost = new Post(obj);
-    await newPost.save();
-    res.json({ message: "Post Uploaded" });
-  } catch (err) {
-    console.log(err);
-    res.json({ message: false });
-  }
-});
-
-// Get all the posts in DB & Infinite Scroll
-router.post("/getPostList", async (req, res) => {
-  try {
-    const fetchPosts = await Post.find({page: req.body._page}, null, {
-      sort: { createdAt: -1 }
-    });
-    res.json({ posts: fetchPosts, length: fetchPosts.length});
-  } catch (err) {
-    console.log(err);
-    res.json({ message: false });
-  }
-});
-
-router.post("/detail", async (req, res) => {
-  try {
-    const _id = req.body._id;
-    const detailedPost = await Post.find({ _id });
-    res.json({ detailedPost });
   } catch (err) {
     console.log(err);
     res.json({ message: false });
